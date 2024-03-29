1bbe1a623e820d2521ad9ea73d4619cf3c4aa353
Moritz Riede <moritz@mriede.com>
2024-03-29 14:54:03 +0100

    fix: point color and length

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup>
-//import aframe from 'aframe';
-//import 'aframe-ar';
+import { ref, onMounted } from 'vue';
 
-import HelloWorld from './components/HelloWorld.vue'
+import * as GeoTIFF from 'geotiff';
+import proj4 from 'proj4';
+import createColormap from 'colormap';
+
+// Returns a range-limited number {n} between {min} and {max}
+// @param {number} n - Current Value
+// @param {number} min - Minimum Value
+// @param {number} max - Maximum Value
+// @returns {number}
+
+const clamp = (n, min, max) => Math.min(Math.max(n, min), max);
 
 const options = {
     geotiff: {
@@ -50,6 +59,8 @@ function getColor(value) {
   const f = clamp((value - min) / (max - min), 0, 1);
   const color = testcm[Math.round(f * (steps - 1))];
   console.log(color);
+
+  return color;
 }
 
 function convertRasterToGlobePoints(rasterValues, bounds, dimensions) {
@@ -72,7 +83,7 @@ function convertRasterToGlobePoints(rasterValues, bounds, dimensions) {
   let data = rasterValues[0];
   let flippedData = new Array(data.length);
 
-  for (let y = 0; y < this.height; y++) {
+  for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
           const value = data[y * width + (width - 1 - x)];
           flippedData[y * width + x] = value;
@@ -88,18 +99,80 @@ function convertRasterToGlobePoints(rasterValues, bounds, dimensions) {
                   value,
                   lat,
                   lng,
-                  size: Math.random() / 3,
+                  size: value * 0.1,
                   color: getColor(value),
-                  //color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)],
               });
           }
       }
   }
 
-  console.log(flippedData)
+  console.log(annotatedRaster);
 
   return annotatedRaster;
 }
+
+onMounted(async () => {
+  const image = await loadGeoTiff(options.geotiff.url);
+
+    /*
+    console.log(await readGeoTiffData(
+        image,
+        options.geotiff.bbox,
+        options.geotiff.resolution[0],
+        options.geotiff.resolution[1],
+    ));
+        */
+    // Convert geographic coordinates to distances using EPSG:3857
+    const bbox = options.geotiff.bbox;
+    const xmin = proj4(options.geotiff.projection, 'EPSG:3857', [bbox[0], bbox[1]]);
+    const xmax = proj4(options.geotiff.projection, 'EPSG:3857', [bbox[2], bbox[3]]);
+
+    const xDistance = xmax[0] - xmin[0];
+    const yDistance = xmax[1] - xmin[1];
+
+    // compute the width of a single hex (gameSize), divided by 2 but multiply by ~1.155 to
+    // account for the below mentioned hexagon wider than taller
+    var gameSize = 20;
+
+    // Adjust board dimensions based on actual distances
+    var width = gameSize;
+    var height = gameSize;
+    // Read the GeoTIFF data into a 1-dimensional array
+    const [oX, oY] = image.getOrigin();
+    const [imageResX, imageResY] = image.getResolution(image);
+
+    let wnd = [
+        Math.round((bbox[0] - oX) / imageResX),
+        Math.round((bbox[1] - oY) / imageResY),
+        Math.round((bbox[2] - oX) / imageResX),
+        Math.round((bbox[3] - oY) / imageResY),
+    ];
+    console.log(wnd);
+    wnd = [
+    Math.min(wnd[0], wnd[2]),
+    Math.min(wnd[1], wnd[3]),
+    Math.max(wnd[0], wnd[2]),
+    Math.max(wnd[1], wnd[3]),
+    ];
+
+    console.log(wnd);
+
+    let raster = await image.readRasters({
+        window: wnd,
+        width: width,
+        height: height,
+        });
+    
+    console.log(raster);
+
+    const pointsData = convertRasterToGlobePoints(raster, options.geotiff.bbox, [options.geotiff.width, options.geotiff.height]);
+
+    document.getElementById('globe').setAttribute('globe', {
+      pointsData,
+      pointAltitude: 'size',
+      pointColor: 'color',
+    });
+})
 </script>
 
 <template>
@@ -111,7 +184,7 @@ function convertRasterToGlobePoints(rasterValues, bounds, dimensions) {
       <a-entity camera></a-entity>
 
       <a-marker type="pattern" url="./patterns/hiro.patt">
-        <a-entity id="globe" scale="0.006 0.006 0.006" globe="
+        <a-entity id="globe" scale="0.025 0.025 0.025" globe="
         globe-image-url: //unpkg.com/three-globe/example/img/earth-dark.jpg;
         bump-image-url: //unpkg.com/three-globe/example/img/earth-topology.png;
       "></a-entity>
